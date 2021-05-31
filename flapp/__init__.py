51ec3c4765557aa2f0dd60b98caa4e22e923d011
nahyun-song <75622751+nahyun-song@users.noreply.github.com>
2021-06-01 01:57:13 +0900

    회원가입, 로그인 완료

--- flapp/__init__.py
+++ flapp/__init__.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session, flash
 from flapp.routes.main_route import bp as main_bp
-from flapp.models import db, migrate
+from flapp.models import db, migrate, User
 import requests
 from bs4 import BeautifulSoup
 
@@ -9,6 +9,7 @@ app.register_blueprint(main_bp)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+app.config['SECRET_KEY'] = '_5#y2L"F4Q8z\n\xec]/'
 
 with app.app_context(): 
     db.init_app(app) 
@@ -18,27 +19,55 @@ with app.app_context():
 
 @app.route('/') 
 def signin():
-    return render_template('signin.html')
+   if session.get('logged_in'):
+        resp_user = User.query.filter_by(userid=session.get('userid')).all()
+        print('return user test')
+        return render_template('signin.html', notes=resp_user, isLogin=True)
 
+   print('Dummy Context!')
+   return render_template('signin.html')
+
+    
 @app.route('/main') 
 def main():
     return render_template('main.html')
 
 @app.route('/signin_done', methods=['POST']) 
 def signin_done():
-    userid = request.form['userid']
-    email = request.form['email']
-    pwd = request.form['pwd']
+    if request.form['userid']:
+        userid = request.form['userid']
+        email = request.form['email']
+        pwd = request.form['pwd']
 
-    return redirect(url_for('main'))
+        user = User(userid=userid, email=email, pwd=pwd)
+        db.session.add(user)
+        db.session.commit()
 
-@app.route('/login') 
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+   return render_template('login.html')
 
-@app.route('/login_done') 
+
+@app.route('/login_done', methods=['POST']) 
 def login_done():
-    pass
+   if request.method == 'POST':
+       userid = request.form['userid']
+       pwd = request.form['pwd']
+       resp = User.query.filter_by(userid=userid, pwd=pwd).first()
+
+       if resp is not None:
+            session.clear()
+            session['user_id'] = resp.userid
+            session['logged_in'] = True
+            print(session)
+            return redirect(url_for('main'))
+       else:
+           error = 'Incorrect ID or Password!'
+           flash(error)
+
+   return redirect(url_for('login'))
 
 
 @app.route('/list') 
@@ -83,4 +112,5 @@ def food():
 
 #어플리케이션 실행하기
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
--- flapp/models.py
+++ flapp/models.py
@@ -6,15 +6,14 @@ from flask_migrate import Migrate
 db = SQLAlchemy() #init파일에서 따로 db를 임포트 해줘야 함
 migrate = Migrate() #init파일에서 연결해줘야 함 (db랑 같이)
 
-
-class Check(db.Model): #__tablename__으로 이름을 지정하지 않아도 클래스 이름의 소문자로 테이블 이름을 자동지정해줌
+class Check(db.Model):
 	userid = db.Column(db.String, primary_key=True)
 	done = db.Column(db.String, nullable=False)
 	date = db.Column(db.Integer, nullable=False)
 	#'이게뭐지' = db.relationship('Post', backref='user', lazy=True)
 
 	def __repr__(self):
-		return f"User id : {self.id}, Check : {self.done}, Date : {self.date}"
+		return f"User id : {self.userid}, Check : {self.done}, Date : {self.date}"
 
 
 class User(db.Model): #데이터 모델을 나타내는 객체 선언  
@@ -22,12 +21,5 @@ class User(db.Model): #데이터 모델을 나타내는 객체 선언
     email = db.Column(db.String(32), nullable=False)
     pwd = db.Column(db.String(8), nullable=False)
 
-    def __init__(self, email, pwd):
-        self.email = email
-        self.set_password(pwd)
-    
-    def set_password(self, password):
-        self.password = generate_password_hash(password)
- 
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
+    def __repr__(self):
+        return f"User id : {self.userid}, Email : {self.email}, Password : {self.pwd}"
